e6b550b29f54b325f374a867a5db997b2e9fdcdc
asus <hassan.shokoohimatin@gmail.com>
2019-12-03 04:03:23 +0330

    final commit...reformatting the code...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,4 @@
 package com.company;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,7 +11,7 @@ public class Main {
         System.out.println("Enter the length of the ArrayList...");
         Scanner in = new Scanner(System.in);
         int length = in.nextInt();
-        if(length!=0) {
+        if (length != 0) {
             System.out.println("Enter the elements of the arraylist...");
             for (int i = 0; i < length; i++) {
                 int value = in.nextInt();
@@ -20,45 +19,40 @@ public class Main {
             }
         }
 
-            myArrayList = function(myArrayList);
+        myArrayList = function(myArrayList);
 
-            if (myArrayList.size()==0){
-                System.out.println("{}");
+        if (myArrayList.size() == 0) {
+            System.out.println("{}");
+        } else {
+
+            for (int i = 0; i < myArrayList.size() - 1; i += 2) {
+                System.out.printf("%s%d%s%d%s\n", "{", myArrayList.get(i), ",", myArrayList.get(i + 1), "}");
             }
-
-            else {
-
-                for (int i = 0; i < myArrayList.size() - 1; i += 2) {
-                    System.out.printf("%s%d%s%d%s\n", "{", myArrayList.get(i), ",", myArrayList.get(i + 1), "}");
-                }
-            }
-
-
-
+        }
 
     }
 
-    public static ArrayList<Integer> function(ArrayList<Integer> array){
 
-        if(array.size()==0)
+    public static ArrayList<Integer> function(ArrayList<Integer> array) {
+
+        if (array.size() == 0)
             return array;
-        else if (array.size()%2==0){
-            for(int i=0;i<array.size()-1;i+=2){
-                if(array.get(i+1)<array.get(i)){
-                    array.remove(i+1);
+        else if (array.size() % 2 == 0) {
+            for (int i = 0; i < array.size() - 1; i += 2) {
+                if (array.get(i + 1) < array.get(i)) {
+                    array.remove(i + 1);
                     array.remove(i);
-                    i=0;
+                    i = 0;
                 }
             }
             return array;
-        }
-        else{
-            array.remove(array.size()-1);
-            for(int i=0;i<array.size()-1;i+=2){
-                if(array.get(i+1)<array.get(i)){
-                    array.remove(i+1);
+        } else {
+            array.remove(array.size() - 1);
+            for (int i = 0; i < array.size() - 1; i += 2) {
+                if (array.get(i + 1) < array.get(i)) {
+                    array.remove(i + 1);
                     array.remove(i);
-                    i=0;
+                    i = 0;
                 }
             }
             return array;
